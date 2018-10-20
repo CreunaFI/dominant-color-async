0cf95b1155ded864839f800149fc9e050b030fd1
Johannes Siipola <johannes@siipo.la>
2018-10-20 10:57:09 +0300

    Add styles to dominant color display on media page, fixes #3

--- src/DominantColorAsync.php
+++ src/DominantColorAsync.php
@@ -9,7 +9,7 @@ class DominantColorAsync
     public function __construct()
     {
         add_action('plugins_loaded', [$this, 'init']);
-        add_action('init', [$this, 'process_handler']);
+        add_action('admin_enqueue_scripts', [$this, 'load_admin_styles']);
         add_filter(
             'wp_generate_attachment_metadata',
             [$this, 'add_image_to_queue'],
@@ -24,30 +24,30 @@ class DominantColorAsync
         $this->process_all = new Process();
     }
 
-    public function media_fields( $form_fields, $post ) {
-        $dominant_color = null;
-        if (get_post_meta($post->ID, 'dominant_color')) {
-            $dominant_color = get_post_meta($post->ID, 'dominant_color', true);
+    public function media_fields($form_fields, $post)
+    {
+
+        $dominant_color = get_post_meta($post->ID, 'dominant_color', true) ?: null;
+
+        $html = ' ';
+
+        if ($dominant_color) {
+            $html = '<div class="dominant-color-async">' .
+                        '<div class="dominant-color-async-circle" style="background-color: ' . $dominant_color . '"></div>' .
+                    '</div>';
         }
+
         $form_fields['dominant-color-async'] = [
             'label' => __('Dominant color', 'dominant-color-async'),
             'input' => 'html',
-            'html' => '<p>' . $dominant_color .'</p>'
+            'html' => $html,
         ];
         return $form_fields;
     }
 
-    public function process_handler()
+    function load_admin_styles()
     {
-        if (!isset($_GET['process']) || !isset($_GET['_wpnonce'])) {
-            return;
-        }
-        if (!wp_verify_nonce($_GET['_wpnonce'], 'process')) {
-            return;
-        }
-        if ('all' === $_GET['process']) {
-            $this->handle_all();
-        }
+        wp_enqueue_style('admin_css_foo', plugins_url('assets/style.css', __DIR__), false, '0.0.1');
     }
 
     public function add_image_to_queue($metadata, $attachment_id)
